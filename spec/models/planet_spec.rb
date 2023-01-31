dd1c8d2b55339d6e78eb6ce3aed738f1793ce690
KaraJH <karas.jones@yahoo.com>
2023-01-31 14:59:30 -0700

    test: Association to planetary systems test

--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Planet do 
   let(:mars) {Planet.create!(name: "Mars", planet_type: "Terrestrial", year_discovered: 1610, confirmed: true)}
-  # let(:solar_system) {PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, metal_rich_star: true)}
+  let(:solar_system) {PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, metal_rich_star: true)}
 
   describe '#attributes/columns' do 
     it 'has a name, type, year discovered and confirmed' do 
@@ -11,8 +11,8 @@ RSpec.describe Planet do
       expect(mars.year_discovered).to eq(1610)
       expect(mars.confirmed).to eq(true)
 
-      #expect(mars.planetary_system_id).to eq(solar_system.id)
+      expect(mars.planetary_system_id).to eq(solar_system.id)
     end
-    #it {should belong_to :planetary_system}
+    it {should belong_to :planetary_system}
   end
 end
