663ccafe32632952340227515f96e3c594d0405f
KaraJH <karas.jones@yahoo.com>
2023-01-31 18:34:36 -0700

    test: Planetary system show page

--- spec/features/planetary_systems/index_spec.rb
+++ spec/features/planetary_systems/index_spec.rb
@@ -6,14 +6,7 @@ RSpec.describe 'Planetary System Index Page' do
     it 'displays the name of each parent record in the system' do 
       the_solar_system = PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, metal_rich_star: true)
       tau_ceti_system = PlanetarySystem.create!(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, metal_rich_star: false)
-        # User Story 1, Parent Index 
-
-        # For each parent table
-        # As a visitor
-        # When I visit '/parents'
-        # Then I see the name of each parent record in the system
        
-
       visit "/planetary_systems"
 
       expect(page).to have_content(the_solar_system.name)
