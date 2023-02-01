983d726804dde4496bc28f1c8e539ac3dddc556f
KaraJH <karas.jones@yahoo.com>
2023-02-01 14:48:34 -0700

    test: Additional test for planetary system planets index

--- spec/features/planetary_systems/planets/index_spec.rb
+++ spec/features/planetary_systems/planets/index_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe "Planetary Systems Planets Index" do
         tau_ceti_h = Planet.create(name: "Tau Ceti H", planet_type: "Super Earth", year_discovered: 2017, confirmed: true, planetary_system_id: tau_ceti_system.id)
 
         visit "planetary_systems/#{the_solar_system.id}/planets"
-        save_and_open_page
 
         expect(page).to have_content(mercury.name)
         expect(page).to have_content(mercury.planet_type)
@@ -44,6 +43,44 @@ RSpec.describe "Planetary Systems Planets Index" do
 
         expect(page).to_not have_content(tau_ceti_e.name)
         expect(page).to_not have_content(tau_ceti_h.planet_type)
+      end
+
+      it 'displays another systems planets' do 
+         the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, metal_rich_star: true)
+        mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
+        venus = Planet.create(name: "Venus", planet_type: "Terrestrial", year_discovered: 1610, confirmed: true, planetary_system_id: the_solar_system.id)
+
+        tau_ceti_system = PlanetarySystem.create(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, metal_rich_star: false)
+        tau_ceti_e = Planet.create(name: "Tau Ceti E", planet_type: "Super Earth", year_discovered: 2017, confirmed: true, planetary_system_id: tau_ceti_system.id)
+        tau_ceti_h = Planet.create(name: "Tau Ceti H", planet_type: "Super Earth", year_discovered: 2017, confirmed: true, planetary_system_id: tau_ceti_system.id)
+
+        visit "planetary_systems/#{tau_ceti_system.id}/planets"
+
+        expect(page).to have_content(tau_ceti_e.name)
+        expect(page).to have_content(tau_ceti_e.planet_type)
+        expect(page).to have_content(tau_ceti_e.year_discovered)
+        expect(page).to have_content(tau_ceti_e.confirmed)
+        expect(page).to have_content(tau_ceti_e.planetary_system.name)
+
+        expect(page).to have_content("Planet Type: #{tau_ceti_e.planet_type}")
+        expect(page).to have_content("Year Discovered: #{tau_ceti_e.year_discovered}")
+        expect(page).to have_content("Confirmed?: #{tau_ceti_e.confirmed}")
+        expect(page).to have_content("Planetary System: #{tau_ceti_e.planetary_system.name}")
+
+        expect(page).to have_content(tau_ceti_h.name)
+        expect(page).to have_content(tau_ceti_h.planet_type)
+        expect(page).to have_content(tau_ceti_h.year_discovered)
+        expect(page).to have_content(tau_ceti_h.confirmed)
+        expect(page).to have_content(tau_ceti_h.planetary_system.name)
+
+        expect(page).to have_content("Planet Type: #{tau_ceti_h.planet_type}")
+        expect(page).to have_content("Year Discovered: #{tau_ceti_h.year_discovered}")
+        expect(page).to have_content("Confirmed?: #{tau_ceti_h.confirmed}")
+        expect(page).to have_content("Planetary System: #{tau_ceti_h.planetary_system.name}")
+
+        expect(page).to_not have_content(venus.name)
+        expect(page).to_not have_content(venus.planet_type)
+
       end
     end
   end
