3fdc8aea1ed13cdc5c5b18537e9f251ea0a31305
KaraJH <karas.jones@yahoo.com>
2023-02-02 17:39:55 -0700

    test: Update planet from show page

--- spec/features/planetary_systems/planets/new_spec.rb
+++ spec/features/planetary_systems/planets/new_spec.rb
@@ -1,18 +1,5 @@
 require 'rails_helper' 
 
-# User Story 13, Parent Child Creation 
-
-# As a visitor
-# When I visit a Parent Children Index page
-# Then I see a link to add a new adoptable child for that parent "Create Child"
-# When I click the link
-# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-# When I fill in the form with the child's attributes:
-# And I click the button "Create Child"
-# Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
-# a new child object/row is created for that parent,
-# and I am redirected to the Parent Childs Index page where I can see the new child listed
-
 RSpec.describe "New Child Creation" do 
   before :each do 
     @the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, metal_rich_star: true)
@@ -28,7 +15,6 @@ RSpec.describe "New Child Creation" do
       click_link "Create Planet"
 
       expect(current_path).to eq("/planetary_systems/#{@the_solar_system.id}/planets/new")
-
     end
 
     it 'it has a form to fill in attributes and submit, create and post to the planetary system planets index' do 
@@ -42,8 +28,6 @@ RSpec.describe "New Child Creation" do
 
       expect(current_path).to eq("/planetary_systems/#{@the_solar_system.id}/planets")
       expect(page).to have_content("Earth")
-      
     end
-
   end
 end
