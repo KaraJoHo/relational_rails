7af77b8e9a238eb908d8d893d257aaa8b351b499
KaraJH <karas.jones@yahoo.com>
2023-02-05 14:33:34 -0700

    feat: Validations with passing test

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,5 +1,9 @@
 class Planet < ApplicationRecord 
   belongs_to :planetary_system
+  validates :name, presence: true
+  validates :planet_type, presence: true 
+  validates :year_discovered, presence: true
+  validates :confirmed, inclusion: [true, false]
 
   def self.only_true 
      Planet.where(confirmed: true)
