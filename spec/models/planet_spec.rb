652f104cbb4ae26a356ba610c89976ac9a63ee94
KaraJH <karas.jones@yahoo.com>
2023-08-29 12:34:47 -0600

    test: Search for lowercase word

--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -58,6 +58,8 @@ RSpec.describe Planet do
       expect(Planet.search_planet_records("Neptune")).to match_array([neptune])
       expect(Planet.search_planet_records("Neptnot")).to match_array(Planet.all)
       expect(Planet.search_planet_records(nil)).to match_array(Planet.all)
+      expect(Planet.search_planet_records("Mercury")).to match_array([mercury])
+      expect(Planet.search_planet_records("mercury")).to match_array([mercury])
     end
   end
 end
